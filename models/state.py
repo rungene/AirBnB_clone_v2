37d7893ca1a93d5f3295c4a0647de4906332df41
rungene <rungenesamuel@gmail.com>
2023-04-20 02:47:09 -0700

    Adding class attributes

--- models/state.py
+++ models/state.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """ State class
+    Attributes:
+        __tablename__:the table name, states
+        name: column containing a string (128 characters)
+        can’t be null
+    """
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
