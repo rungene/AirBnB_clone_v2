a90fb4bb88bee15e6983c58d83299e0472315a4f
rungene <rungenesamuel@gmail.com>
2023-04-20 11:56:28 -0700

    Adding a new engine DBStorage. Add private class attributes:

---
+++ models/engine/db_storage.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module defines a class to manage database storage for hbnb clone"""
+
+class DBStorage:
+    """This class manages db storage of hbnb models
+    _engine:(private)
+    __session:(private)
+    """
+    __engine = None
+    __session = None
