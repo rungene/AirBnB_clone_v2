02d11a3a061f27aceff81f7e862ede0d02611f68
rungene <rungenesamuel@gmail.com>
2023-04-20 03:58:25 -0700

    getter attribute cities that returns the list of City instances with state_id equals to the current State.id => It will be the FileStorage relationship between State and City

--- models/state.py
+++ models/state.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from sqlalchemy import Column, Stringi, Integer
 from sqlalchemy.orm import relationship
 from models.city import City
 
@@ -14,7 +14,25 @@ class State(BaseModel, Base):
         can’t be null
         cities:a relationship with the class City
     """
+    id = Column(Integer, primary_key=True)
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
     cities = relationship('City', backref='state',
                           cascade="all, delete-orphan")
+
+    @property
+    def cities(self):
+        """
+        getter attribute
+
+        Return:
+            list of City instances with state_id equals
+            to the current State.id
+        """
+        from models import storage
+        my_list = []
+        my_cities = storage.all(City).values
+        for city in my_cities:
+            if self.id == city.state_id:
+                my_list.append(city)
+        return my_list
