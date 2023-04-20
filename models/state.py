206402b41736fb56b0d86260d28917e516d8e5ea
rungene <rungenesamuel@gmail.com>
2023-04-20 03:08:36 -0700

    Adding class attribute cities

--- models/state.py
+++ models/state.py
@@ -2,6 +2,8 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.city import City
 
 
 class State(BaseModel, Base):
@@ -10,6 +12,9 @@ class State(BaseModel, Base):
         __tablename__:the table name, states
         name: column containing a string (128 characters)
         can’t be null
+        cities:a relationship with the class City
     """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
+    cities = relationship('City', backref='state',
+                          cascade="all, delete-orphan")
