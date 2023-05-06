ae977d4787c28b2bd43a971937da14d1ea8fc779
rungene <rungenesamuel@gmail.com>
2023-05-06 00:33:44 +0000

    Corrections

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # A Bash script that sets up your web servers for the deployment of web_static
-mkdir -p /data/
-mkdir -p /data/web_static/
-mkdir -p /data/web_static/releases/
-mkdir -p /data/web_static/shared/
-mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
 
 sudo echo "<!DOCTYPE html>
 <html>
