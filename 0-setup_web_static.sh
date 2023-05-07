f789e8d21a376646e262dc11ad2da93df2280aa2
rungene <rungenesamuel@gmail.com>
2023-05-07 17:31:09 +0000

    Corrections

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 # A Bash script that sets up your web servers for the deployment of web_static
-sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
+sudo apt-get -y  update
+#Check if Nginx is installed
+if ! [ -x "$(command -v nginx)" ]; then
+  # install Nginx since it is not installed
+  sudo apt-get -y install nginx
+fi
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
 
@@ -14,20 +17,11 @@ sudo echo "<html>
   </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
 
-# Give ownership of the /data/ folder to the ubuntu user AND group 
-sudo chown -R ubuntu:ubuntu /data/
-
 # Create a symbolic link /data/web_static/current linked to the /data/web_static/releases/test/ folder.
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-# update the package list
-sudo apt-get -y  update
-
-#Check if Nginx is installed
-if ! [ -x "$(command -v nginx)" ]; then
-  # install Nginx since it is not installed
-  sudo apt-get -y install nginx
-fi
+# Give ownership of the /data/ folder to the ubuntu user AND group 
+sudo chown -R ubuntu:ubuntu /data/
 
 # configure Nginx to serve web_static content
 sudo sed -i "33i \\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
