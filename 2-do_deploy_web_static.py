4ade077cefba6037e5b02b1b7394177a729b7ac5
rungene <rungenesamuel@gmail.com>
2023-05-08 02:17:26 -0700

    Removing def do_pack():

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,25 +8,7 @@ from datetime import datetime
 import os
 
 
-env.hosts = ['35.174.209.69, 18.208.120.192']
-env.user = 'ubuntu'
-
-
-def do_pack():
-    """ generates a .tgz archive from the contents of the web_static folder"""
-    try:
-        if not os.path.exists("versions"):
-            os.makedirs("versions")
-
-        date = datetime.now().strftime('%Y%m%d%H%M%S')
-        path_archive = "versions/web_static_{}.tgz".format(date)
-
-        local("tar -cvzf {} web_static".format(path_archive))
-
-        return path_archive
-
-    except Exception:
-        return None
+env.hosts = ['35.174.209.69', '18.208.120.192']
 
 
 def do_deploy(archive_path):
