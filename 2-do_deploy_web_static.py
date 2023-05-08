b0a14ab69147ed44d238e5eb9192154c7e6c6581
rungene <rungenesamuel@gmail.com>
2023-05-07 19:31:24 -0700

    Fabric script (based on the file 1-pack_web_static.py) that distributes an archive to your web servers, using the function do_deploy:

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -24,3 +24,20 @@ def do_deploy(archive_path):
     """
     if not archive_path:
         return False
+        
+    result = put(archive_path, "/tmp/")
+    if result.failed:
+        return False
+
+    filename = archive_path.split("/")[-1]
+    folder_name = "/data/web_static/releases/" + filename.split(".")[0]
+    
+    run("sudo mkdir -p {}".format(folder_name))
+    run("sudo tar -xzf /tmp/{} -C {}".format(filename, folder_name))
+    run("sudo mv {}/web_static/* {}/".format(folder_name, folder_name))
+    run("sudo rm -rf {}/web_static".format(folder_name))
+    run("sudo rm -rf /data/web_static/current")
+    run("sudo ln -s {} /data/web_static/current".format(folder_name))
+    print('New version deployed!')
+    
+    return True
