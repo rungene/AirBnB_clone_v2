3186fc788a63fd9d2735d1017fdf006075a5f82b
Timilehin Olusa <olusatimmie07@gmail.com>
2023-05-11 14:45:46 -0700

    Debuggrd

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,12 +2,14 @@
 # This script sets up your web servers for the deployment of web_static
 sudo apt-get -y update
 sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
 sudo mkdir -p /data/ /data/web_static/ /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/ /data/web_static/releases/test
 content="
 <html>
 <h1> HELLO. THIS IS MY WEBSITE </h1>
 </html>"
+sudo touch /data/web_static/releases/test/index.html
 echo $content | sudo tee /data/web_static/releases/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
