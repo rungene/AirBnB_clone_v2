4e4c778ee0b1c44f7e5d7f993708c326d9823d00
rungene <rungenesamuel@gmail.com>
2023-05-21 02:41:02 -0700

    a script that starts a Flask web application:

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+1-hello_route module.
+a script that starts a Flask web application
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hnb():
+    """starts a Flask web application
+    Return:
+        string:“Hello HBNB!”
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hnb():
+    """starts a Flask web application
+    Return:
+        string:“HBNB”
+    """
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
