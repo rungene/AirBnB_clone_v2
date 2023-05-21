59c6bdb0a14f1c2c9d71fb731ddc8662ad96e2a0
rungene <rungenesamuel@gmail.com>
2023-05-21 07:05:29 -0700

    a script that starts a Flask web application.Adding route /number_template/<n>: display a HTML page only if n is an integer

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python3
+"""
+5-hello_route module.
+a script that starts a Flask web application
+"""
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hnb():
+    """starts a Flask web application
+    Return:
+        string:“Hello HBNB!”
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hnb():
+    """starts a Flask web application
+    Adding specific route
+    Return:
+        string:“HBNB”
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text=None):
+    """starts a Flask web application
+    Dynamic inputted text
+    Return:
+        string:“C” + formated text
+        replace underscore _ symbols with a space
+    """
+    text = text.replace('_', ' ')
+    return 'C {}'.format(text)
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text='is cool'):
+    """starts a Flask web application
+    Dynamic inputted text
+    Return:
+        string:“Python” + formated text
+        replace underscore _ symbols with a space
+    """
+    text = text.replace('_', ' ')
+    return 'Python {}'.format(text)
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def n_number(n):
+    """starts a Flask web application
+    Dynamic inputted text
+    Return:
+        string:n is a number” only if n is an integer
+    """
+    return '{} is a number'.format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def render_number_template(n):
+    """starts a Flask web application
+    Dynamic inputted text
+    Return:
+         display a HTML page only if n is an integer
+    """
+    return render_template('5-number.html', num=n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
